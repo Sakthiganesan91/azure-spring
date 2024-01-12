513ee0ba8939977568ea1a8401f3b04058f5eebb
Sakthiganesan <sakthiganesan158@gmail.com>
2024-01-12 11:09:07 +0530

    first commit

---
+++ src/main/java/com/example/azu/AzuApplication.java
@@ -0,0 +1,13 @@
+package com.example.azu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/azu/AzuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.azu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
